1f2e2a34103ebaf7742f5608d929aa5ce01306b5
alandler <46603394+alandler@users.noreply.github.com>
2020-06-16 22:10:23 -0400

    Some of the pages work now

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import request
+from flask import Flask,request,render_template  
 from datetime import datetime
 import todoist
 import sqlite3
@@ -11,31 +10,28 @@ db = 'data.db' #[Item ID, Project ID, Content, Due Date, Priority, Time Estmate]
 api = todoist.TodoistAPI('f84b92c505121b40a3489b4904b7eeb99ad11b7b')
 api.sync()
 
+about_message = "BUTT"
+
 def update_database():
     #access database and table
     conn = sqlite3.connect(db) 
     c = conn.cursor()  
-    c.execute('''CREATE TABLE IF NOT EXISTS timetable (item_id real PRIMARY KEY, project_id real, content text, due_date timestamp, priority real, time_estimate real);''')
+    c.execute('''CREATE TABLE IF NOT EXISTS timetable (item_id real PRIMARY KEY, time_estimate real);''')
 
     for item in api.state['items']:
-        try:
-            four = datetime.strptime(item["due"]["date"], "%Y-%m-%s")
-        except:
-            four = datetime(year = 2050, month = 1, day = 1, hour = 0, minute = 0, second = 0)
         one = item["id"]
-        two = item["project_id"]
-        three = item["content"]
-        five = item["priority"]
-
-        c.execute('''INSERT OR REPLACE INTO timetable (item_id, project_id, content, due_date, priority, time_estimate) \
-            VALUES (?, ?, ?, ?, ?, 0);''', (one,two,three,four,five))
+        c.execute('''INSERT OR REPLACE INTO timetable (item_id, time_estimate) \
+            VALUES (?, ?, ?, ?, ?, 0);''', (one,))
     conn.commit()
     conn.close()
 
-#update_database()
+update_database()
 
-@app.route('/all')
-def all():
+@app.route('/')
+def home():
+    data = retrieve_from_database()
+
+def retrieve_from_database():
     conn = sqlite3.connect(db) 
     c = conn.cursor()  
     results = c.execute('''SELECT item_id, time_estimate FROM timetable;''').fetchall()
@@ -46,10 +42,9 @@ def all():
     conn.close() # close connection to database
     return out
     
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/about')
+def about():
+    return render_template("about.html", about_message = about_message)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
